ca305822a49a815bd97368681a871fc16971472d
Ubuntu <ubuntu@ip-172-31-1-244.ec2.internal>
2026-02-17 15:40:52 +0000

    Agrego funcion es_primo y pruebas unitarias con pytest

---
+++ test_primo.py
@@ -0,0 +1,22 @@
+# test_primo.py
+import pytest
+from Primo import es_primo
+
+@pytest.mark.parametrize("n, esperado", [
+    (-10, False),
+    (-1, False),
+    (0, False),
+    (1, False),
+    (2, True),
+    (3, True),
+    (4, False),
+    (5, True),
+    (9, False),
+    (11, True),
+    (25, False),
+    (29, True),
+    (97, True),
+    (121, False),
+])
+def test_es_primo(n, esperado):
+    assert es_primo(n) == esperado
